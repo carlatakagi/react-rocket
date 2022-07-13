b6aeaa1ed45ca18a2d9b8f7d79efde1a2f7d27e5
carlatakagi <carlatakagi@gmail.com>
2022-07-12 21:43:54 -0400

    feat: style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 export function Home() {
 
   return (
-    <div className="Home">
+    <div className="container">
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
